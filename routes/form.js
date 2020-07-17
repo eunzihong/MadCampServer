455caf80cd5f5e947afcf86eee497a458e7d15a0
Eunji Hong <eunjihong.3698@gmail.com>
2020-07-17 19:19:16 +0000

    server env with mongodb&mongoose

---
+++ routes/form.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res, next){
+    res.render('form', {
+        name : 'Eunji',
+        blog : 'eunzihong.github.io',
+        homepage : 'github.io/eunzihong'
+    });
+});
+router.post('/', function(req, res, next){
+    res.json(req.body);
+});
+module.exports = router;
