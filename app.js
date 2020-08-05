d40929a142b5770d2bf82f66ea518a51d821af26
Eunji Hong <eunjihong.3698@gmail.com>
2020-08-06 01:35:39 +0900

    week2 server

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+const user = require('./models/user');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+mongoose.connect('mongodb://localhost/madcamp2');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Model
+// var User = require('./models/user');
 
-module.exports = app;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 80;
+
+app.use('/', require('./routes'));
+app.use(express.static('uploads'));
+
+
+var server = app.listen(port, function(){
+    console.log("Express server has started on port " + port)
+   });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,178 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const multer = require('multer');
+// const upload = multer({dest: 'images/'}) //dest : 저장 위치
+const path = require('path');
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'images/');
+      },
+      filename: function (req, file, cb) {
+        cb(null, new Date().valueOf() + path.extname(file.originalname));
+      }
+    }),
+  });
+const PythonShell = require('python-shell');
+const options = {
+    mode : 'json',
+    pythonOptions: ['-u'],
+    pythonPath : '../torch'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+};
+
+router.post('/api/register', function(req, res){
+    console.log('/register');
+    User.findOne({ uid: req.body.uid }, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(user) return res.status(400).json({ message: 'already registered'});
+        const new_user = new User(req.body);
+        new_user.save();
+        res.json({});
+    });
+});
+
+router.post('/api/contacts/post/:uid', function(req, res){
+    console.log('/contacts/post');
+    User.findOne({ uid: req.params.uid}, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(!user) return res.status(404).json({ error: 'user not found' });
+        user.contacts.push(req.body);
+        user.save();
+        res.json(req.body);
+        console.log('post contact done');
+    });
+});
+
+router.post('/api/images/post/:uid', upload.single('image'),(req,res)=>{
+    console.log('post image');
+    var image = req.file;
+    User.findOne({uid:req.params.uid}, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(!user) return res.status(404).json({ error: 'user not found' });
+        user.images.push(req.file.filename);
+        user.save();
+        res.json('success');
+        console.log(req.file.filename);
+        console.log('user image list updated');
+    });
+});
+
+router.get('/api/images/delete/:uid/:filename', function(req, res){
+    console.log('delete image');
+    console.log(req.params.uid);
+    console.log(req.params.filename);
+    console.log(__dirname);
+
+    var uid = req.params.uid;
+    var filename = req.params.filename;
+
+    User.findOne({uid:uid}, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(!user) return res.status(404).json({ error: 'user not found' });
+        user.images.pull(filename);
+        user.save();
+        console.log('image name unlinked from user image list');
+        fs.unlink(path.join(__dirname, '..', 'images', filename), (err)=>{
+            if(err) return res.status(500).json({error:err});
+            console.log("image file delete");
+            res.json('Ok, Thank you!');
+        });
+    });
+    
+});
+
+
+
+router.get('/api/login/:uid', function(req, res){
+    console.log('/login');
+    User.findOne({uid: req.params.uid}, function(err, user){
+        console.log(req.params.uid);
+        if(err) return res.status(500).json({error: err}); 
+        if(!user) {
+            console.log("no user data");
+            return res.status(400).json({message: 'not registered yet'})};
+        res.json("login success");
+        console.log('login success');
+    });
+});
+
+router.get('/api/contacts/:uid', function(req, res){
+    console.log('/contacts');
+    User.findOne({ uid: req.params.uid }, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(!user) return res.status(404).json({ error: 'user not found' });
+        res.json(user.contacts);
+    });
+});
+
+router.get('/api/images/:uid', function(req, res){
+    console.log('/images');
+    User.findOne({ uid: req.params.uid }, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(!user) return res.status(404).json({ error: 'user not found' });
+        res.json(user.images);
+        console.log(user.images);
+        console.log('imagessssssss');
+    });
+})
+
+router.get('/api/users', function(req, res){
+    console.log('/users');
+    User.find(function(err, users){
+        if(err) return res.status(500).json({error: err});
+        res.json(users);
+        console.log("users:"+json(users));
+    })
+});
+
+router.get('/api/images/get/:filename', function(req, res){
+    console.log('image load');
+    var filename =  req.params.filename;
+    res.sendFile(path.join(__dirname, '..', 'images', filename));
+    console.log('image send');
+});
+
+router.post('/api/images/post/:uid', upload.single('image'),(req,res)=>{
+    console.log('post image');
+    var image = req.file;
+    User.findOne({uid:req.params.uid}, function(err, user){
+        if(err) return res.status(500).json({error: err});
+        if(!user) return res.status(404).json({ error: 'user not found' });
+        user.images.push(req.file.filename);
+        user.save();
+        res.json('success');
+        console.log(req.file.filename);
+        console.log('user image list updated');
+    });
+});
+
+router.post('/api/torch/inference', upload.single('image'),(req,res)=>{
+    console.log('inference image');
+    var image = req.file;
+    var imageStr = image.toString("base64");
+    console.log(__dirname);
+
+    infpyshell = new PythonShell.PythonShell(path.join(__dirname, '..', 'torch', 'inference.py'));
+
+    infpyshell.send(imageStr);
+
+    infpyshell.on('result', function(result){
+        res.json(result);
+        console.log(result);
+    });
+
+    infpyshell.end(err => {
+        if(err){
+            res.status(500).json({error: err});
+            console.log(err);
+        }
+    });
+    console.log('inference done');
+
 });
 
 module.exports = router;
